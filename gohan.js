c37bbc5a87cf1ef297da06fbf03479c34fb749a2
faith_ut <vecuto@gmail.com>
2021-04-16 21:47:36 +0900

    Update gohan.js

--- gohan.js
+++ gohan.js
@@ -7,3 +7,12 @@
   }
 }
 </script>
+
+$(()=> {
+   let dir = location.href.split("/");  
+   let dir2 = "g-interview/entry/";  
+   let dir3 = dir[dir.length -2];  
+   let dir4 = dir[dir.length -1];  
+　let mgurl = dir2 + dir3 + "/" + dir4;
+　alert(mgurl);
+});  
